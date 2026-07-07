ea5391502e59096f76aebdd0f8339e39b906c25a
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-07 04:44:26 +0200

    Expose Riccati sweep API

--- src/steady_state_combined/__init__.py
+++ src/steady_state_combined/__init__.py
@@ -12,16 +12,32 @@ from .ellipsoidal import (
     spectral_radius,
     stepwise_trace_steady_state,
 )
+from .riccati import (
+    GainOptimizedProblem,
+    RiccatiResult,
+    deterministic_gain_optimized_problem,
+    grid_optimize_riccati,
+    riccati_update,
+    solve_fixed_alpha_riccati,
+    stepwise_gain_trace_recursion,
+)
 
 __all__ = [
     "FixedGainProblem",
+    "GainOptimizedProblem",
+    "RiccatiResult",
     "bounded_error_check",
     "criterion_value",
+    "deterministic_gain_optimized_problem",
+    "grid_optimize_riccati",
     "grid_optimize_weights",
     "line_search_to_nonmyopic",
     "nonmyopic_weights",
+    "riccati_update",
     "solve_adjoint",
+    "solve_fixed_alpha_riccati",
     "solve_fixed_gain_steady_state",
     "spectral_radius",
+    "stepwise_gain_trace_recursion",
     "stepwise_trace_steady_state",
 ]
